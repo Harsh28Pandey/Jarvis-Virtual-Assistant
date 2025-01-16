a3761e792e30d865cb639d57140d95163a8f828e
Harsh Pandey <146448821+Harsh28Pandey@users.noreply.github.com>
2025-01-16 15:12:59 +0530

    main.py

--- main.py
+++ main.py
@@ -19,52 +19,6 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-
-
-# def speak(text):
-#     tts = gTTS(text)
-#     tts.save("temp.mp3")
-
-#     # Intialize Pygame mixer
-#     pygame.mixer.init()
-
-#     # Load the saved mp3 file
-#     pygame.mixer.music.load("temp.mp3")
-
-#     # Play the loaded mp3 file
-#     pygame.mixer.music.play()
-
-#     # Keep the program running until the music stops playing
-#     while pygame.mixer.music.get_busy():
-#         pygame.time.Clock().tick(10)
-
-#     pygame.mixer.music.unload()
-#     os.remove("temp.mp3")
-
-
-
-
-# def aiProcess(command):
-#     client = OpenAI(
-#     api_key="",
-#     )
-
-#     completion = client.chat.completions.create(
-#         model="gpt-3.0-turbo",
-#         messages=[
-#             {"role": "system", "content": "You are a Virtual assistant named Jarvis skilled in general taska like Alexa and Google Cloud Give short responses please"},
-#             {
-#                 "role": "user",
-#                 "content": command
-#             }
-#         ]
-#     )
-
-#     return completion.choices[0].message.content
-
-
-
-
 def processCommand(c):
     if("open google" in c.lower()):
         webbrowser.open("https://www.google.com")
@@ -82,33 +36,6 @@ def processCommand(c):
         song = c.lower().split(" ")[1]
         link = musicLibrary.music[song]
         webbrowser.open(link)
-
-
-
-
-
-    # elif "news" in c.lower():
-    #     r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey={newsapi}")
-    #     if r.status_code == 200:
-    #         # Parse the JSON reponse
-    #         data = r.json()
-
-    #         # Extract the articles
-    #         articles = data.get('articles', [])
-
-    #         # Print the headlines
-    #         for article in articles:
-    #             speak(article['title'])
-
-
-    # else:
-    #     # Let OpenAI handle the request
-    #     output = aiProcess(c)
-    #     speak(output)
-
-
-
-
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
